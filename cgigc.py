5642bc488512b3821a7b7c809d4c98540989b92f
Dario Giovannetti <dev@dariogiovannetti.net>
2016-09-24 23:40:34 +0800

    Implement basic functionality

--- cgigc.py
+++ cgigc.py
@@ -20,46 +20,27 @@
 # http://python-future.org/
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
+# TODO: Test builtins.super
 # from builtins import super
 
 # https://docs.python.org/2.6/library/internet.html
 # https://docs.python.org/2.6/library/cgi.html
 # http://www.tutorialspoint.com/python/python_cgi_programming.htm
 import cgi
+import sys
 import os
 
 
-def debug(display=False, logdir=None, context=5, format='html'):
-    """
-    Shortcut for cgitb.enable().
-    """
-    # https://docs.python.org/2.6/library/cgitb.html
-    import cgitb
-    # TODO: Does cgitb rotate the log files, or do they have to be
-    #       cleaned up explicitly?
-    cgitb.enable(display=1 if display else 0, logdir=logdir,
-                 context=context, format=format)
-
-
-class Request(object):
-    def __init__(self, keep_blank_form_values=False):
+class _Request(object):
+    def __init__(self, keep_blank_form_values):
         """
         Store the HTTP request data, e.g. GET or POST data.
         """
+        self.redirect_url = os.environ['REDIRECT_URL']
+
         # FieldStorage must be instantiated only once
         self.form = cgi.FieldStorage(
                 keep_blank_values=keep_blank_form_values)
-
-    def get_env(self):
-        """
-        TODO
-        """
-        # TODO: Write more specific methods
-        html = '<ul>Environment:'
-        for name, value in os.environ.items():
-            html += "<li><b>{0}</b>: {1}</li>".format(name, value)
-        html += '</ul>'
-        return html
 
     def get_form(self):
         """
@@ -78,29 +59,40 @@ class Request(object):
         return html
 
 
-class Response(object):
-    def __init__(self, header={'Content-type': 'text/html'}):
+class _Response(object):
+    DEFAULT_HEADERS = {'Status': '200 OK',
+                       'Content-type': 'text/html'}
+
+    def __init__(self):
         """
-        Set up the response HTTP header.
+        Set up and send the HTTP response, headers and body.
         """
-        # The header will be consumed only once the first time that self.out()
-        # is called
-        self.header = '\n'.join(': '.join((name, value))
-                                for name, value in header.items())
+        self.set_headers(self.DEFAULT_HEADERS.copy())
+
+    def set_headers(self, headers):
+        # TODO: Use an OrderedDict?
+        #       Is it allowed to have two headers with the same name?
+        self.headers = headers
+
+        # TODO: Implement get_headers(), update_headers()/add_headers(),
+        #       remove_headers()
 
     @staticmethod
     def test():
         """
         Output cgi.test() and other information.
 
-        Static method, doesn't need to instantiate CgigC.
+        Static method, it prints its own headers.
         """
-        import sys
         import platform
 
-        # cgi.test() also prints the header, so it must come first
+        # cgi.test() prints directly, including the headers, so it must come
+        # first
+        # TODO: Find a way to return the content instead of printing it
+        #       directly, like any other response function
         cgi.test()
 
+        # Note that from here on the content is normally returned
         html = '<h3>Python sys.version</h3>\n'
         html += '<div>{0}</div>\n'.format(sys.version)
 
@@ -112,19 +104,137 @@ class Response(object):
         html += '<h3>Python platform.platform()</h3>\n'
         html += '<div>{0}</div>\n'.format(platform.platform())
 
-        print(html)
+        return html
 
-    def out(self, content, end=''):
-        """
-        Output content, with the HTTP header if still needed.
-        """
-        if self.header:
-            content = '\n\n'.join((self.header, content))
-            self.header = None
-        print(content, end=end)
+    def _send(self, body):
+        compiled_headers = '\n'.join(': '.join((name, value))
+                                     for name, value in self.headers.items())
+        print(compiled_headers, body, sep='\n\n', end='')
 
-    def outn(self, content):
+
+class CgigC(object):
+    @staticmethod
+    def debug():
         """
-        Same as self.out(), but adding a line break at the end.
+        Display uncaught exceptions' tracebacks. This static method is supposed
+        to be run as early as possible in the code, especially before
+        instantiating this very class. It is insecure to use this method in
+        production.
         """
-        self.out(content, end='\n')
+        # TODO: Enable debugging if a special url or parameter is sent?
+        #       Or maybe check if the parameter is there only inside
+        #         'display_uncaught_exception'?
+        #       Or maybe encrypt the traceback before sending it?
+        # TODO: Implement logging for use in production
+
+        def display_uncaught_exception(type_, value, traceback):
+            import traceback as _m_traceback
+            # TODO: Set a "500 Internal Server Error" status
+            print('Content-type: text/plain\n')
+            _m_traceback.print_exception(type_, value, traceback,
+                                         file=sys.stdout)
+            sys.exit(1)
+
+        sys.excepthook = display_uncaught_exception
+
+    # cgitb seems to give more trouble than help...
+    # @staticmethod
+    # def debug(display=False, logdir=None, context=5, format='html'):
+    #     """
+    #     Shortcut for cgitb.enable(). This static method is supposed to be
+    #     run as early as possible in the code, especially before instantiating
+    #     this very class.
+    #     """
+    #     # h_ttps://docs.python.org/2.6/library/cgitb.html
+    #     import cgitb
+    #     # TO_DO: Does cgitb rotate the log files, or do they have to be
+    #     #       cleaned up explicitly?
+    #     cgitb.enable(display=1 if display else 0, logdir=logdir,
+    #                  context=context, format=format)
+
+    def __init__(self, keep_blank_form_values=False):
+        """
+        The main application.
+        """
+        # TODO: Implement logging for use in production
+        # TODO: How to address HTTP errors and redirects?
+
+        self.request = _Request(keep_blank_form_values)
+
+    def route(self, url):
+        """
+        Decorator that tests the url and possibly immediately calls the
+        function and exits the program, thus preventing any following code,
+        including further routes, from being even tested.
+
+        Designed to be used in projects that mostly rely on non-Python template
+        files, such as Jinja, and only use short functions to set the templates
+        up.
+
+        Example:
+
+            @app.route(UrlExact('/hello_world.htm'))
+            def hello_world(app):
+                return 'Hello World!'
+        """
+        def decorator(function):
+            def inner(*args, **kwargs):
+                return function(*args, **kwargs)
+            self._serve(url, inner)
+            return inner
+        return decorator
+
+    def serve(self, url, resource):
+        """
+        Test a set of urls in the given order and possibly immediately run the
+        respective resource's 'make' function or method, or call the resource
+        directly, then exit the program, thus preventing any following code,
+        including the next rules, from being even tested.
+
+        Designed to be used in projects that mostly construct the pages using
+        Python code, relying on several functions, classes or modules.
+
+        Example:
+
+            class HelloWorld(object):
+                def serve(self, app):
+                    return 'Hello World!'
+
+
+            app.serve(UrlExact('/hello_world.htm'), HelloWorld())
+        """
+        try:
+            function = resource.make
+        except AttributeError:
+            function = resource
+        self._serve(url, function)
+
+    def _serve(self, url, function):
+        testres = url.test(self)
+        if testres:
+            self.response = _Response()
+            body = function(self, *url.args, **url.kwargs)
+            self.response._send(body)
+            # No need to test the remaining rules
+            sys.exit(0)
+
+
+class _UrlTest(object):
+    def __init__(self):
+        super(_UrlTest, self).__init__()
+        self.args = ()
+        self.kwargs = {}
+
+
+class UrlDefault(_UrlTest):
+    def test(self, app):
+        return True
+
+
+class UrlExact(_UrlTest):
+    def __init__(self, url):
+        super(UrlExact, self).__init__()
+        self.url = url
+
+    def test(self, app):
+        return self.url == app.request.redirect_url
