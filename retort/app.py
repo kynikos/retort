9df6361026ae954a5a7c77724cad693320732696
Dario Giovannetti <dev@dariogiovannetti.net>
2016-09-26 13:50:05 +0800

    Rename to Retort

--- retort/app.py
+++ retort/app.py
@@ -1,20 +1,20 @@
-# CgigC - Check the status of code repositories under a root directory.
+# Retort - Simple Python CGI framework.
 # Copyright (C) 2016 Dario Giovannetti <dev@dariogiovannetti.net>
 #
-# This file is part of CgigC.
+# This file is part of Retort.
 #
-# CgigC is free software: you can redistribute it and/or modify
+# Retort is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# CgigC is distributed in the hope that it will be useful,
+# Retort is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with CgigC.  If not, see <http://www.gnu.org/licenses/>.
+# along with Retort.  If not, see <http://www.gnu.org/licenses/>.
 
 # The module must also support Python 2.6
 # http://python-future.org/
@@ -126,7 +126,7 @@ class Response(object):
         print(self._compile_headers(), body, sep='\n\n', end='')
 
 
-class CgigC(object):
+class Retort(object):
     DEFAULT_RESPONSE = Response
 
     @staticmethod
